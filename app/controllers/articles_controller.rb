fe3e6342d82c0788e257a382fe2f8b239449c224
Shahzad Hafeez <shahzadhafeez63@gmail.com>
2018-07-17 04:55:08 +0500

    create form from web

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,38 @@
 class ArticlesController < ApplicationController
 
-def index
+def home
 end
 
 def about
 end
+
+def show
+	@article=Article.find(params[:id])
+end
+
+def new
+	@article=Article.new
+end
+
+def create
+	@article=Article.new(params_article)
+	if @article.save
+	    flash[:notice] = "Article was succesfully created"
+	    redirect_to article_path(@article)
+    else
+    	render 'new'
+
+    end
+end
+
+
+
+private
+
+def params_article
+	params.require(:article).permit(:title, :description)
+end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'articles#index'
-get '/about' => 'articles#about'
+get 'articles/about' => 'articles#about'
+#get 'articles/new' => 'articles#new'
+resources :articles
 
 end
